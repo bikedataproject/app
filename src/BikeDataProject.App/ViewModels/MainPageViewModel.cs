06d904febdff9b298400c1d931b3268c2ed09119
Lukas Carton <carton.lukas@gmail.com>
2020-07-09 13:39:24 +0200

    Add Start button

---
+++ src/BikeDataProject.App/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+using Xamarin.Forms;
+
+namespace BikeDataProject.App.ViewModels
+{
+    public class MainPageViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public MainPageViewModel()
+        {
+
+        }
+
+        public Command StartTrackingCommand { get; }
+    }
+}
