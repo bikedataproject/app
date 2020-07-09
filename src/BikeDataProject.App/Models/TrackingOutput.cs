500a723b76485935f4be11cf6a1ca40a2ac70b02
Lukas Carton <carton.lukas@gmail.com>
2020-07-09 14:14:48 +0200

    Add geolocation

---
+++ src/BikeDataProject.App/Models/TrackingOutput.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BikeDataProject.App.Models
+{
+    public class TrackingOutput
+    {
+        public double Longitude { get; set; }
+        public double Latitude { get; set; }
+        public double Altitude { get; set; }
+        public DateTimeOffset DateTimeOffset { get; set; }
+    }
+}
--- src/BikeDataProject.App/ViewModels/TrackingPageViewModel.cs
+++ src/BikeDataProject.App/ViewModels/TrackingPageViewModel.cs
@@ -1,4 +1,10 @@
-﻿using System.ComponentModel;
+﻿using BikeDataProject.App.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using Xamarin.Essentials;
 using Xamarin.Forms;
 
 namespace BikeDataProject.App.ViewModels
@@ -9,11 +15,57 @@ namespace BikeDataProject.App.ViewModels
 
         public TrackingPageViewModel()
         {
+            continueTimer = true;
+            GetTrackingOutputs = new List<TrackingOutput>();
+
             StopTrackingCommand = new Command(async () => {
+                continueTimer = false;
                 await Application.Current.MainPage.Navigation.PopAsync();
+            });
+
+            Device.StartTimer(TimeSpan.FromMilliseconds(5000), () =>
+            {
+                Task.Factory.StartNew(async () =>
+                {
+                    var location = await GetLocation();
+                    if (location != null)
+                    {
+
+                        var trackingOutput = new TrackingOutput { Longitude = location.Longitude, Latitude = location.Latitude, DateTimeOffset = location.Timestamp };
+                        if (location.Altitude != null)
+                        {
+                            trackingOutput.Altitude = (double)location.Altitude;
+                        }
+
+
+                        GetTrackingOutputs.Add(trackingOutput);
+                        Console.WriteLine($"Accuracy: {location.Accuracy}, Time: {location.Timestamp}, Long: {location.Longitude}, lat: {location.Latitude}");
+                    }
+                });
+
+                return continueTimer;
             });
         }
 
         public Command StopTrackingCommand { private set; get; }
+
+        private List<TrackingOutput> GetTrackingOutputs;
+
+        private bool continueTimer;
+
+        private async Task<Location> GetLocation()
+        {
+            try
+            {
+                var request = new GeolocationRequest(GeolocationAccuracy.Best);
+                var location = await Geolocation.GetLocationAsync(request);
+                return location;
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine($"Something is wrong: {ex.Message}");
+                return null;
+            }
+        }
     }
 }
