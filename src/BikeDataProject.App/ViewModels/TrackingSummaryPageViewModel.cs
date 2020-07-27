070628a2d04438b358e5dfe93cd58e0fea1f8453
Lukas Carton <carton.lukas@gmail.com>
2020-07-27 15:40:47 +0200

    Added documentation for TrackingSummaryPageViewModel

--- src/BikeDataProject.App/ViewModels/TrackingSummaryPageViewModel.cs
+++ src/BikeDataProject.App/ViewModels/TrackingSummaryPageViewModel.cs
@@ -175,13 +175,15 @@ namespace BikeDataProject.App.ViewModels
             SendTrackCommand = new Command(async () =>
             {
                 Guid userId;
-                Debug.WriteLine($"#### {gender} - {ageRange} # {bikeType} - {tripPurpose}");
 
-                // Need check if user has internet connection
+                // Check if user has internet connectivity
                 var connectivity = Connectivity.NetworkAccess;
-                Debug.WriteLine($"-------- Connectivity: {connectivity}");
+
                 if (connectivity == NetworkAccess.Internet)
                 {
+                    // Check if the user already has a userId stored in the local database
+                    // If true => use the already existing userId
+                    // If false => request a userId from the server
                     List<UserInfo> userInfo = await App.Database.GetUserInfos();
                     if (userInfo.Count == 0)
                     {
@@ -194,8 +196,10 @@ namespace BikeDataProject.App.ViewModels
                         userId = userInfo[0].UserIdentifier;
                     }
 
+                    // Send the data (all the locations) to the server
                     var result = await SendTracks(userId);
 
+                    // Delete all the locations if the data is succesfully send to the server
                     if (result)
                     {
                         await DeleteAllLocationsAsync();
@@ -235,6 +239,9 @@ namespace BikeDataProject.App.ViewModels
         //        return mTelephonyMgr.DeviceId;
         //}
 
+        /// <summary>
+        /// The total distance of the cycled bike ride (in kilometers)
+        /// </summary>
         double distance;
         public double Distance
         {
@@ -247,6 +254,9 @@ namespace BikeDataProject.App.ViewModels
             }
         }
 
+        /// <summary>
+        /// The total time of the cycled bike ride
+        /// </summary>
         TimeSpan elapsedTime;
         public TimeSpan ElapsedTime
         {
@@ -539,6 +549,9 @@ namespace BikeDataProject.App.ViewModels
             }
         }
 
+        /// <summary>
+        /// Initialize every option category with 'rather not share'
+        /// </summary>
         private void InitializeButtons()
         {
             String notShare = "Rather not share";
@@ -557,6 +570,11 @@ namespace BikeDataProject.App.ViewModels
 
         }
 
+        /// <summary>
+        /// Send all the locations and userId to the server
+        /// </summary>
+        /// <param name="userId"></param>
+        /// <returns></returns>
         private async Task<bool> SendTracks(Guid userId)
         {
             List<LocPost> locations = MapLocationsForApi(await GetLocationsAsync());
@@ -567,27 +585,38 @@ namespace BikeDataProject.App.ViewModels
             });
         }
 
+        /// <summary>
+        /// Get all the locations
+        /// </summary>
+        /// <returns>A list containing all the locations</returns>
         private async Task<List<Loc>> GetLocationsAsync()
         {
             return await App.Database.GetLocationsAsync();
         }
 
+        /// <summary>
+        /// Ask if user really wants to discard the data
+        /// </summary>
+        /// <returns>True if the users presses 'Yes'; false is the user presses 'No'</returns>
         private async Task<bool> DiscardData()
         {
-            List<RideInfo> rideInfos = await App.Database.GetRideInfoAsync();
-            foreach (RideInfo ride in rideInfos) 
-            {
-                Debug.WriteLine($"-------- ID: {ride.ID} Distance: {ride.AmountOfKm} Time: {ride.ElapsedTime}");
-            }
-
             return await Application.Current.MainPage.DisplayAlert("Discard data?", "Are you sure you want to discard the data you just collected?", "Yes", "No");
         }
 
+        /// <summary>
+        /// Navigates to the root page (MainPage)
+        /// </summary>
         private async Task NavigateToMainPage()
         {
             await Application.Current.MainPage.Navigation.PopToRootAsync();
         }
 
+        /// <summary>
+        /// Maps the list of Loc-objects to a list of LocPost objects.
+        /// Loc-objects are used to internally store the data, LocPost objects are used to send the data to the server
+        /// </summary>
+        /// <param name="locations">List of loc objects</param>
+        /// <returns>Returns a list of LocPost objects</returns>
         private List<LocPost> MapLocationsForApi(List<Loc> locations)
         {
             List<LocPost> locPosts = new List<LocPost>();
@@ -606,6 +635,10 @@ namespace BikeDataProject.App.ViewModels
             return locPosts;
         }
 
+        /// <summary>
+        /// Delete all the locations from the last ride and delete the info of the last ride (distance and elapsed time)
+        /// </summary>
+        /// <returns></returns>
         private async Task DeleteLastRide()
         {
             var lastRide = await App.Database.GetLastRideInfoId();
@@ -614,6 +647,10 @@ namespace BikeDataProject.App.ViewModels
             await App.Database.DeleteRideAsync(lastRide[0].ID);
         }
 
+        /// <summary>
+        /// Delete all the locations
+        /// </summary>
+        /// <returns></returns>
         private async Task DeleteAllLocationsAsync() 
         {
             await App.Database.DeleteAllLocationsAsync();
